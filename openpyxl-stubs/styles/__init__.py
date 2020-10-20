65065f3d1825506201b9ae0f062d9460871609b2
Martin Thoma <info@martin-thoma.de>
2020-10-20 08:20:47 +0200

    Remove styles/__init__.py

--- openpyxl-stubs/styles/__init__.py
+++
@@ -1,8 +0,0 @@
-from openpyxl.styles.alignment import Alignment
-from openpyxl.styles.borders import Border, Side
-from openpyxl.styles.colors import Color
-from openpyxl.styles.fills import PatternFill, GradientFill, Fill
-from openpyxl.styles.fonts import Font, DEFAULT_FONT
-from openpyxl.styles.numbers import NumberFormatDescriptor, is_date_format, is_builtin
-from openpyxl.styles.protection import Protection
-from openpyxl.styles.named_styles import NamedStyle
